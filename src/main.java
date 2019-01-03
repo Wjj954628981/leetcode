a1b1a0df99e0510b6751070e2a1c6d3bf587fd9c
王佳颉 <wangjiajie@qiyi.com>
2019-01-03 10:41:49 +0800

    twoSum

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args)
+    {
+        solution sol = new solution();
+        int input[] = {1,2,3};
+        int result[] = sol.twoSum(input, 4);
+        System.out.println(Arrays.toString(result));
+    }
+}
