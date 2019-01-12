9ee77182fe207fc670b891695a0ec39e05230a54
wangjiajie <wangjiajie@qiyi.com>
2019-01-12 23:32:48 +0800

    canJump

--- src/solution.java
+++ src/solution.java
@@ -144,4 +144,15 @@ public class solution {
         }
         return ret;
     }
+
+    //Q55:跳跃游戏
+    public boolean canJump(int[] nums) {
+        int n = 1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]>=n) n=1;
+            else n++;
+            if(i==0&&n!=1) return false;
+        }
+        return true;
+    }
 }
