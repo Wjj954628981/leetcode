6ebab50ea9da2b4e56f8a3700135c4a7a5ddc8dd
wangjiajie <wangjiajie@qiyi.com>
2019-04-18 16:53:13 +0800

    maxAncestorDiff

--- src/solution.java
+++ src/solution.java
@@ -684,4 +684,60 @@ public class solution {
         }
         return ans;
     }
+
+    //Q5030:节点与其祖先之间的最大差值
+    //给定二叉树的根节点 root，找出存在于不同节点 A 和 B 之间的最大值 V，其中 V = |A.val - B.val|，且 A 是 B 的祖先。
+    //
+    //（如果 A 的任何子节点之一为 B，或者 A 的任何子节点是 B 的祖先，那么我们认为 A 是 B 的祖先）
+    //
+    //
+    //
+    //示例：
+    //
+    //
+    //
+    //输入：[8,3,10,1,6,null,14,null,null,4,7,13]
+    //输出：7
+    //解释：
+    //我们有大量的节点与其祖先的差值，其中一些如下：
+    //|8 - 3| = 5
+    //|3 - 7| = 4
+    //|8 - 1| = 7
+    //|10 - 13| = 3
+    //在所有可能的差值中，最大值 7 由 |8 - 1| = 7 得出。
+    //
+    //
+    //提示：
+    //
+    //树中的节点数在 2 到 5000 之间。
+    //每个节点的值介于 0 到 100000 之间。
+    List<Integer> onePath = new ArrayList<>();
+    int maxDiff = Integer.MIN_VALUE;
+    public int maxAncestorDiff(TreeNode root) {
+        dfs(root);
+        return maxDiff;
+    }
+    //深度优先遍历
+    public void dfs(TreeNode node) {
+        if (node == null) return;
+        onePath.add(node.val);
+        if(node.left == null && node.right == null){
+            maxDiff = Math.max(maxDiff, Collections.max(onePath) - Collections.min(onePath));
+        }
+        dfs(node.left);
+        dfs(node.right);
+        onePath.remove(onePath.size()-1);
+    }
+    public int maxAncestorDiff2(TreeNode root) {
+        return maxdiff(root, root.val, root.val);
+    }
+    //递归求左右分支最大最小值
+    private int maxdiff(TreeNode root, int min, int max){
+        if(root==null)return 0;
+        min = Math.min(min, root.val);
+        max = Math.max(max, root.val);
+        int left = maxdiff(root.left, min, max);
+        int right = maxdiff(root.right, min, max);
+        return Math.max(Math.max(Math.abs(root.val-min), Math.abs(root.val-max)), Math.max(left, right));
+    }
 }
