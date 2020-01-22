ca9e20602e87d91dc3a66a030383ec3e8b998274
wangjiajie <wangjiajie@qiyi.com>
2020-01-22 14:39:14 +0800

    169:majorityElement

--- .leetcode/169.求众数.go
+++ .leetcode/169.求众数.go
@@ -2,8 +2,21 @@
  * @lc app=leetcode.cn id=169 lang=golang
  *
  * [169] 求众数
+ * 投票法
  */
 func majorityElement(nums []int) int {
-
+	var count = 0
+	var candidate = 0
+	for _, v := range nums {
+		if count == 0 {
+			candidate = v
+		}
+		if v == candidate {
+			count += 1
+		} else {
+			count -= 1
+		}
+	}
+	return candidate
 }
 
